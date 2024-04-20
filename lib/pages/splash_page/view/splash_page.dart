baac38ad795b944bc6b5de46d2e61de07ff83fa0
AkbarFelda <akbarfelda65@gmail.com>
2024-04-20 22:07:55 +0700

    feat: [AF] add splash-page

--- lib/pages/splash_page/view/splash_page.dart
+++ lib/pages/splash_page/view/splash_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:warmindo_admin_ui/pages/splash_page/controller/splash_controller.dart';
+import 'package:warmindo_admin_ui/utils/themes/image_themes.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Get.put(SplashController());
+    return Scaffold(
       body: Center(
-        child: Text('Splash Page'),
+        child: Image.asset(
+          Images.splashLogo,
+          width: 220,
+          height: 220,
+        ),
       ),
     );
   }
