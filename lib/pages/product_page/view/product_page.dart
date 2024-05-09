8e26e90692ed8f0452d57a7adc394df054f1184a
AkbarFelda <akbarfelda65@gmail.com>
2024-05-09 21:20:11 +0800

    style: [AF] add mediaquery product-page

--- lib/pages/product_page/view/product_page.dart
+++ lib/pages/product_page/view/product_page.dart
@@ -37,114 +37,118 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     // Mendapatkan ukuran layar
-    final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Manage Product',
       ),
-      body: Column(
-        children: [
-          CategoryWidget(
-            onCategorySelected: (selectedCategory) {
-              filterProducts(selectedCategory);
-            },
-          ),
-          SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredProducts.length,
-              itemBuilder: (context, index) {
-                final product = filteredProducts[index];
-                return ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(2),
-                      topLeft: Radius.circular(2),
-                    ),
-                    child: SizedBox(
-                      width: screenWidth * 0.15,
-                      height: screenHeight * 0.15,
-                      child: Image.asset(
-                        product.image,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    product.name,
-                    style: titleproductTextStyle,
-                  ),
-                  subtitle: Text(
-                    'Rp ${product.price.toStringAsFixed(0)} | ${product.stock} in stock',
-                    style: titleproductTextStyle,
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return ReusableDialog(
-                                title: "Edit",
-                                content:
-                                    "Apakah Kamu yakin ingin mengubah data?",
-                                cancelText: "Tidak",
-                                confirmText: "Iya",
-                                onCancelPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                onConfirmPressed: () {
-                                  Navigator.of(context).pop();
-                                  Get.toNamed(Routes.LOGIN_PAGE);
-                                },
-                                cancelButtonColor:
-                                    ColorResources.primaryColorLight,
-                                confirmButtonColor: ColorResources.buttonedit,
-                                dialogImage: Image.asset(Images.askDialog),
-                              );
-                            },
-                          );
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return ReusableDialog(
-                                title: "Delete",
-                                content:
-                                    "Apakah Kamu yakin ingin menghapus data?",
-                                cancelText: "Tidak",
-                                confirmText: "Iya",
-                                onCancelPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                onConfirmPressed: () {
-                                  Navigator.of(context).pop();
-                                  Get.toNamed(Routes.LOGIN_PAGE);
-                                },
-                                cancelButtonColor:
-                                    ColorResources.primaryColorLight,
-                                confirmButtonColor: ColorResources.buttondelete,
-                                dialogImage: Image.asset(Images.askDialog),
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                );
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.01, vertical: screenHeight * 0.01),
+        child: Column(
+          children: [
+            CategoryWidget(
+              onCategorySelected: (selectedCategory) {
+                filterProducts(selectedCategory);
               },
             ),
-          ),
-        ],
+            SizedBox(height: screenHeight * 0.02),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredProducts.length,
+                itemBuilder: (context, index) {
+                  final product = filteredProducts[index];
+                  return ListTile(
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(2),
+                        topLeft: Radius.circular(2),
+                      ),
+                      child: SizedBox(
+                        width: screenWidth * 0.15,
+                        height: screenHeight * 0.15,
+                        child: Image.asset(
+                          product.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      product.name,
+                      style: titleproductTextStyle,
+                    ),
+                    subtitle: Text(
+                      'Rp ${product.price.toStringAsFixed(0)} | ${product.stock} in stock',
+                      style: titleproductTextStyle,
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return ReusableDialog(
+                                  title: "Edit Product",
+                                  content:
+                                      "Apakah Kamu yakin ingin mengubah data?",
+                                  cancelText: "Tidak",
+                                  confirmText: "Iya",
+                                  onCancelPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  onConfirmPressed: () {
+                                    Get.toNamed(Routes.EDIT_PRODUCT_PAGE);
+                                  },
+                                  cancelButtonColor:
+                                      ColorResources.primaryColorLight,
+                                  confirmButtonColor: ColorResources.buttonedit,
+                                  dialogImage: Image.asset(Images.askDialog),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return ReusableDialog(
+                                  title: "Delete",
+                                  content:
+                                      "Apakah Kamu yakin ingin menghapus data?",
+                                  cancelText: "Tidak",
+                                  confirmText: "Iya",
+                                  onCancelPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  onConfirmPressed: () {
+                                    Navigator.of(context).pop();
+                                    // Get.toNamed(Routes.LOGIN_PAGE);
+                                  },
+                                  cancelButtonColor:
+                                      ColorResources.primaryColorLight,
+                                  confirmButtonColor:
+                                      ColorResources.buttondelete,
+                                  dialogImage: Image.asset(Images.askDialog),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -155,13 +159,12 @@ class _ProductPageState extends State<ProductPage> {
                 title: "Add Product",
                 content: "Apakah Kamu yakin ingin menambah data?",
                 cancelText: "Tidak",
-                confirmText: "Iya" ,
+                confirmText: "Iya",
                 onCancelPressed: () {
                   Navigator.of(context).pop();
                 },
                 onConfirmPressed: () {
-                  Navigator.of(context).pop();
-                  Get.toNamed(Routes.LOGIN_PAGE);
+                  Get.toNamed(Routes.ADD_PRODUCT_PAGE);
                 },
                 cancelButtonColor: ColorResources.primaryColorLight,
                 confirmButtonColor: ColorResources.buttonadd,
