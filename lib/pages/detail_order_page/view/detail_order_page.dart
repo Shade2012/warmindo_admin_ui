4ca0a8395291bf4a2ca0f1ac664ad40b7a5fc302
AkbarFelda <akbarfelda65@gmail.com>
2024-05-09 19:41:27 +0700

    style: [AF] add mediaquery

--- lib/pages/detail_order_page/view/detail_order_page.dart
+++ lib/pages/detail_order_page/view/detail_order_page.dart
@@ -11,17 +11,19 @@ class DetailOrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            width: 32,
-            height: 32,
+            width: screenHeight * 0.08,
+            height: screenHeight * 0.08,
             decoration: BoxDecoration(
               color: Colors.white,
               shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(screenHeight * 0.025),
               boxShadow: [
                 BoxShadow(
                   color: Colors.black.withOpacity(0.2),
@@ -43,51 +45,43 @@ class DetailOrderPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.symmetric(
+            horizontal: screenHeight * 0.02, vertical: screenHeight * 0.02),
         child: ListView(
           children: [
             Container(
-              width: 331,
-              height: 98,
+              width: screenHeight * 0.86,
+              height: screenHeight * 0.118,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Image.asset(
                     Images.splashLogo,
-                    width: 83,
+                    width: screenHeight * 0.120,
                   ),
-                  SizedBox(width: 10),
-                  Expanded(
+                  SizedBox(width: screenHeight * 0.05),
+                  Flexible(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text('Warmindo Anggrek Muria',
                             style: nameRestoTextStyle),
-                        SizedBox(height: 5),
-                        Text(
-                          'Id Pesanan',
-                          style: idOrderTextStyle,
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Tanggal Pesanan',
-                          style: dateOrderTextStyle,
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Status Pesanan',
-                          style: statusOrderTextStyle,
-                        ),
-                        SizedBox(height: 5),
+                        SizedBox(height: screenHeight * 0.0110),
+                        Text('Id Pesanan', style: idOrderTextStyle),
+                        SizedBox(height: screenHeight * 0.0110),
+                        Text('Tanggal Pesanan', style: dateOrderTextStyle),
+                        SizedBox(height: screenHeight * 0.0125),
+                        Text('Status Pesanan', style: statusOrderTextStyle),
                       ],
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: screenHeight * 0.04),
             ReceiptScreen(),
-            SizedBox(height: 20),
+            SizedBox(height: screenHeight * 0.04),
             ElevatedButton(
               onPressed: () {},
               child: Text('Cetak Bukti'),
