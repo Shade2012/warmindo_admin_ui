acb368f6548fbdc4dc32ae72362b4f59fcb83de9
AkbarFelda <akbarfelda65@gmail.com>
2024-05-28 23:29:04 +0700

    fix: [AF] fix overflow on sales-detail-page
    git commit "fix : [AF] resolve conflict"

---
+++ lib/repository/warmindo_repository.dart
@@ -0,0 +1,11 @@
+import 'package:warmindo_admin_ui/common/global_variables.dart';
+
+class FoodApi {
+  static String apiUrl = GlobalVariables.apiUrl;
+  static var getallProductList = '$apiUrl/api/menus';
+  static var getFoodList = '$apiUrl/api/menus/filter/makanan';
+  static var getDrinkList = '$apiUrl/api/menus/filter/minuman';
+  static var getSnackList = '$apiUrl/api/menus/filter/snack';
+  static var getToppingList = '$apiUrl/api/menus/filter/topping';
+  static var getVariantList = '$apiUrl/api/menus/filter/varian';
+}
